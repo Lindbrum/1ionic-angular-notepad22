fb1bb336064f0da5300439eda02279e6f807ae6a
poxmarco <45430944+poxmarco@users.noreply.github.com>
2021-01-16 16:38:31 +0100

    check box - Test 1

--- src/app/nota/nota.page.ts
+++ src/app/nota/nota.page.ts
@@ -3,43 +3,78 @@ import {NotesService} from '../services/notes.service';
 import {AlertController, NavController} from '@ionic/angular';
 import {ModalController} from '@ionic/angular';
 import {ModalPage} from '../modal/modal.page';
+import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
-  selector: 'app-nota',
-  templateUrl: './nota.page.html',
-  styleUrls: ['./nota.page.scss'],
+    selector: 'app-nota',
+    templateUrl: './nota.page.html',
+    styleUrls: ['./nota.page.scss'],
 })
 export class NotaPage implements OnInit {
-  title: any;
-  content: any;
-  color: string;
 
-  constructor(public notesService: NotesService, private alertCtrl: AlertController, private navCtrl: NavController,
-              private modalCtrl: ModalController ) {
-}
-ngOnInit(){
-  this.notesService.load();
-}
+    constructor(public notesService: NotesService, private alertCtrl: AlertController, private navCtrl: NavController,
+                private modalCtrl: ModalController, private formBuilder: FormBuilder ) {
+
+
+
+
+
+
+        this.myForm = formBuilder.group({
+            player1: ['', Validators.required]
+        });
+    }
+    title: any;
+    content: any;
+    color: string;
+    public myForm: FormGroup;
+    private playerCount = 1;
+
+    gesf: any;
+
+
+
+
+
+
+    ngOnInit(){
+        this.notesService.load();
+    }
+
 // funzione chiamata dal pulsante "salva"
-addNote(){
-  this.notesService.createNote(this.title, this.content, this.color);
-  this.notesService.load();
-  this.navCtrl.back();
+    addNote(){
+        this.notesService.createNote(this.title, this.content, this.color);
+        this.navCtrl.back();
+    }
 
-}
+    goToLogin(){
+        this.navCtrl.navigateRoot('login');
+    }
 
-goToLogin(){
-  this.navCtrl.navigateRoot('login');
-}
+    async showModal(){
+        const modal = await this.modalCtrl.create({
+            component: ModalPage
+        }).then( modals => {
+            modals.present();
+        });
+    }
 
-async showModal(){
-    const modal = await this.modalCtrl.create({
-      component: ModalPage
-    }).then( modals => {
-        modals.present();
-    });
 
-}
 
+
+
+
+
+
+
+
+    addControl(){
+        this.playerCount++;
+        this.myForm.addControl('player' + this.playerCount, new FormControl('', Validators.required));
+    }
+
+    removeControl(control){
+        this.myForm.removeControl(control.key);
+    }
 
 }
